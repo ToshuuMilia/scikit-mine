55a3d7985583b0a092cfd87972c29d1154457904
Gregory Martin <gregory.martin@irisa.fr>
2022-01-12 11:14:53 +0100

    feat/lcm_neighbours: Added code to compute the pattern with LCM while taking into account neighbour patterns.

--- skmine/itemsets/lcm.py
+++ skmine/itemsets/lcm.py
@@ -10,11 +10,14 @@ as described in `http://lig-membres.imag.fr/termier/HLCM/hlcm.pdf`
 
 from collections import defaultdict
 from itertools import takewhile
+from typing import Union, List, Dict, Any, Tuple, FrozenSet, Generator, Iterable, Optional
 
 import numpy as np
 import pandas as pd
 from joblib import Parallel, delayed
-from sortedcontainers import SortedDict
+from pandas import DataFrame
+from sortedcontainers import SortedDict, SortedKeyList
+
 
 from ..utils import _check_min_supp
 from ..utils import filter_maximal
@@ -230,6 +233,288 @@ class LCM(BaseMiner, DiscovererMixin):
                     # new pattern and its associated tids
                     new_p_tids = (p_prime, tids.intersection(ids))
                     yield from self._inner(new_p_tids, new_limit, depth + 1)
+
+
+# Just for type comprehension.
+Item = Any
+Pattern = FrozenSet
+
+
+class LCMNeighbours(BaseMiner, DiscovererMixin):
+    """
+    Linear time Closed item set Miner.
+
+    LCM can be used as a **generic purpose** miner, yielding some patterns
+    that will be later submitted to a custom acceptance criterion.
+
+    It can also be used to simply discover the set of **closed itemsets** from
+    a transactional dataset.
+
+    Parameters
+    ----------
+    min_supp: int or float, default=0.2
+        The minimum support for itemsets to be rendered in the output
+        Either an int representing the absolute support, or a float for relative support
+        Default to 0.2 (20%)
+
+    max_depth: int, default=20
+        Maximum depth for exploration in the search space.
+        When going into recursion, we check if the current depth
+        is **strictly greater** than `max_depth`.
+        If this is the case, we stop.
+        This can avoid cumbersome computation.
+        A **root node is considered of depth 0**.
+
+    n_jobs : int, default=1
+        The number of jobs to use for the computation. Each single item is attributed a job
+        to discover potential itemsets, considering this item as a root in the search space.
+        **Processes are preffered** over threads.
+
+    References
+    ----------
+    .. [1]
+        Takeaki Uno, Masashi Kiyomi, Hiroki Arimura
+        "LCM ver. 2: Efficient mining algorithms for frequent/closed/maximal itemsets", 2004
+
+    .. [2] Alexandre Termier
+        "Pattern mining rock: more, faster, better"
+
+    Examples
+    --------
+
+    >>> from skmine.itemsets import LCM
+    >>> from skmine.datasets.fimi import fetch_chess
+    >>> chess = fetch_chess()
+    >>> lcm = LCM(min_supp=2000)
+    >>> patterns = lcm.fit_discover(chess)      # doctest: +SKIP
+    >>> patterns.head()                         # doctest: +SKIP
+        itemset support
+    0      (58)    3195
+    1  (11, 58)    2128
+    2  (15, 58)    2025
+    3  (17, 58)    2499
+    4  (21, 58)    2224
+    >>> patterns[patterns.itemset.map(len) > 3]  # doctest: +SKIP
+    """
+
+    def __init__(self, *, min_supp=0.2, max_depth=20, n_jobs=1, verbose=False):
+        _check_min_supp(min_supp)
+        self.min_supp: Union[int, float] = min_supp  # provided by user
+        self.max_depth: int = int(max_depth)
+        self._min_supp: Union[int, float] = _check_min_supp(self.min_supp)
+
+        # Holds the information about in which transaction is present each item.
+        self.item_to_tids_: SortedDict[Item, Bitmap] = SortedDict()
+
+        # Total number of transactions.
+        self.n_transactions_: int = 0
+
+        # Holds the information about the neighbours of each item.
+        self._item_to_neighbours: Dict[Item, List[Item]] = defaultdict(list)
+
+        self.ctr = 0
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+
+    def fit(self, D, y=None):
+        """
+        fit LCM on the transactional database, by keeping records of singular items
+        and their transaction ids.
+
+        Parameters
+        ----------
+        D: pd.Series or iterable
+            a transactional database. All entries in this D should be lists.
+            If D is a pandas.Series, then `(D.map(type) == list).all()` should return `True`
+
+        Raises
+        ------
+        TypeError
+            if any entry in D is not iterable itself OR if any item is not **hashable**
+            OR if all items are not **comparable** with each other.
+        """
+        self.n_transactions_ = 0  # reset for safety
+
+        # Initializes the dictionary holding the information about in which transaction is present each item.
+        item_to_tids: Dict[Any, Bitmap] = defaultdict(Bitmap)
+        for transaction in D:  # For all transactions
+            for item in transaction:  # For all items in the current transaction
+                # Add the n° of current transaction in the list of transaction for the current item.
+                item_to_tids[item].add(self.n_transactions_)
+            self.n_transactions_ += 1  # Update n° of transaction for the next one.
+
+        if isinstance(self.min_supp, float):
+            # make support absolute if needed
+            self._min_supp = self.min_supp * self.n_transactions_
+
+        # Here low support items are removed.
+        # low_supp_items = [k for k, v in item_to_tids.items() if len(v) < self._min_supp]
+        # for item in low_supp_items:
+        #     del item_to_tids[item]
+
+        self.item_to_tids_ = SortedDict(item_to_tids)
+        return self
+
+    def discover(self, *, return_tids=False, return_depth=False):
+        """Return the set of closed itemsets, with respect to the minium support
+
+        Parameters
+        ----------
+        D : pd.Series or Iterable
+            The input transactional database
+            Where every entry contain singular items
+            Items must be both hashable and comparable
+
+        return_tids: bool, default=False
+            Either to return transaction ids along with itemset.
+            Default to False, will return supports instead
+
+        return_depth: bool, default=False
+            Either to return depth for each item or not.
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame with the following columns
+                ==========  =================================
+                itemset     a `tuple` of co-occured items
+                support     frequence for this itemset
+                ==========  =================================
+
+            if `return_tids=True` then
+                ==========  =================================
+                itemset     a `tuple` of co-occured items
+                tids        a bitmap tracking positions
+                ==========  =================================
+
+            if `return_depth` is `True`, then a `depth` column is also present
+
+        Example
+        -------
+        >>> from skmine.itemsets import LCM
+        >>> D = [[1, 2, 3, 4, 5, 6], [2, 3, 5], [2, 5]]
+        >>> LCM(min_supp=2).fit_discover(D)
+             itemset  support
+        0     (2, 5)        3
+        1  (2, 3, 5)        2
+        >>> LCM(min_supp=2).fit_discover(D, return_tids=True, return_depth=True) # doctest: +SKIP
+             itemset       tids depth
+        0     (2, 5)  [0, 1, 2]     0
+        1  (2, 3, 5)     [0, 1]     1
+        """
+
+        # Sorts the entries of the SortedDict such that the item with the most support is 1st.
+        supp_sorted_items = sorted(
+            self.item_to_tids_.items(), key=lambda e: len(e[1]), reverse=True
+        )
+
+        # Explore the tree of patterns for each single item and knowing in which transaction they are in.
+        dfs = Parallel(n_jobs=self.n_jobs, prefer="processes")(
+            delayed(self._explore_root)(item, tids) for item, tids in supp_sorted_items
+        )
+
+        # make sure we have something to concat
+        dfs.append(pd.DataFrame(columns=["itemset", "tids", "depth"]))
+        df = pd.concat(dfs, axis=0, ignore_index=True)
+        if not return_tids:
+            df.loc[:, "support"] = df["tids"].map(len).astype(np.uint32)
+            df.drop("tids", axis=1, inplace=True)
+
+        if not return_depth:
+            df.drop("depth", axis=1, inplace=True)
+        return df
+
+    def _explore_root(self, item: Item, tids: Bitmap) -> DataFrame:
+        # Mines the pattern of the given item.
+        it = self._inner((frozenset(), tids), item)
+
+        # Recovers the patterns to create the result DataFrame.
+        df = pd.DataFrame(data=it, columns=["itemset", "tids", "depth"])
+
+        if self.verbose and not df.empty:
+            print("LCM found {} new itemsets from item : {}".format(len(df), item))
+        return df
+
+    def _inner(self, p_tids: Tuple[Pattern, Bitmap], limit: Item, depth=0):
+
+        # If we reached the maximum depth, stop the pattern generation.
+        if depth >= self.max_depth:
+            return
+
+        pattern, pattern_transaction_ids = p_tids
+
+        # project and reduce DB w.r.t P
+        cp: Generator[Item] = (
+            item
+            for item, item_transction_ids in reversed(self.item_to_tids_.items())
+            if pattern_transaction_ids.issubset(item_transction_ids)
+            if item not in pattern
+        )
+
+        # items are in reverse order, so the first consumed is the max
+        max_k = next(takewhile(lambda e: e >= limit, cp), None)
+
+        if max_k and max_k == limit:
+            p_prime: Pattern = (
+                pattern | set(cp) | {max_k}
+            )  # max_k has been consumed when calling next()
+            # sorted items in ouput for better reproducibility
+            yield tuple(sorted(p_prime)), pattern_transaction_ids, depth
+
+            candidates: Iterable = self.item_to_tids_.keys() - p_prime
+            candidates = candidates[: candidates.bisect_left(limit)]
+            for new_limit in candidates:
+                ids = self.item_to_tids_[new_limit]
+                candidate_pattern = p_prime | frozenset([new_limit])
+                # if pattern_transaction_ids.intersection_len(ids) >= self._min_supp:  # TODO Modify here
+
+                assert self._is_neighbourfrequent(candidate_pattern, self._min_supp) == (pattern_transaction_ids.intersection_len(ids) >= self._min_supp), f"{self._is_neighbourfrequent(candidate_pattern, self._min_supp)} == {(pattern_transaction_ids.intersection_len(ids) >= self._min_supp)} - pattern: {pattern} - candidatepattern: {candidate_pattern} - new_limit: {new_limit} - interseclen: {pattern_transaction_ids.intersection_len(ids)} - ptids: {pattern_transaction_ids} - ids: {ids} - minsupp: {self._min_supp}"
+
+                if self._is_neighbourfrequent(candidate_pattern, self._min_supp):
+                    assert self._min_supp != 100, f"{candidate_pattern}"
+                    # new pattern and its associated tids
+                    new_p_tids = (p_prime, pattern_transaction_ids.intersection(ids))
+                    yield from self._inner(new_p_tids, new_limit, depth + 1)
+
+    def _is_neighbourfrequent(self, pattern: Pattern, min_supp: Optional[int] = None) -> bool:
+        """Computes whether a pattern is expected to be frequent while taking into account its neighbour patterns."""
+
+        total_support: int = 0
+
+        for pattern_candidate in self._generate_allneighbours(pattern):
+
+            # Initialize the transaction ids bitmap with one random item.
+            p_transaction_ids: Bitmap = self.item_to_tids_[next(iter(pattern_candidate))]
+
+            # For each item, creates the intersection of the transaction ids. At the end of the loop the transaction ids
+            # represents in which transactions the pattern appears.
+            # If at one moment the number of transaction is zero, it means the support of the pattern is zero.
+            for item in pattern_candidate:
+                p_transaction_ids = p_transaction_ids.intersection(self.item_to_tids_[item])
+                if len(p_transaction_ids) == 0:
+                    break
+
+            total_support += len(p_transaction_ids)
+
+            # If the minimum support threshold is met, then the pattern given in parameter is frequent.
+            if total_support >= min_supp:
+                break
+
+        return total_support >= min_supp
+
+    def _generate_allneighbours(self, pattern: Pattern) -> Generator[Pattern, None, None]:
+        """A generator to generate all the neighbour patterns, including the original one."""
+        # If the received pattern is empty, return it.
+        if len(pattern) == 0:
+            yield frozenset()
+        else:
+            # Takes one arbitrary item from the pattern.
+            current_item: Item = next(iter(pattern))
+
+            # Generate all possible combination of neighbours pattern, including the original one.
+            for neighbour_item in [current_item] + self._item_to_neighbours[current_item]:
+                for candidate_pattern in self._generate_allneighbours(pattern - frozenset([current_item])):
+                    yield frozenset({neighbour_item}) | candidate_pattern
 
 
 class LCMMax(LCM):
---
+++ skmine/itemsets/tests/test_lcm_neighbours.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import pytest
+import numpy as np
+
+from ..lcm import LCMNeighbours
+
+
+D = [
+    [1, 2, 3, 4, 5, 6],
+    [2, 3, 5],
+    [2, 5],
+    [1, 2, 4, 5, 6],
+    [2, 4],
+    [1, 4, 6],
+    [3, 4, 6],
+]
+
+true_item_to_tids = {
+    1: {0, 3, 5},
+    2: {0, 1, 2, 3, 4},
+    3: {0, 1, 6},
+    4: {0, 3, 4, 5, 6},
+    5: {0, 1, 2, 3},
+    6: {0, 3, 5, 6},
+}
+
+true_patterns = pd.DataFrame(
+    [  # from D with min_supp=3
+        [{2}, 5],
+        [{4}, 5],
+        [{2, 4}, 3],
+        [{2, 5}, 4],
+        [{4, 6}, 4],
+        [{1, 4, 6}, 3],
+        [{3}, 3],
+    ],
+    columns=["itemset", "support"],
+)
+
+true_patterns.loc[:, "itemset"] = true_patterns.itemset.map(tuple)
+
+NULL_RESULT = (None, None, 0)
+
+
+def test_lcm_fit():
+    lcm = LCMNeighbours(min_supp=3)
+    lcm.fit(D)
+
+    for item in lcm.item_to_tids_.keys():
+        assert set(lcm.item_to_tids_[item]) == true_item_to_tids[item]
+
+
+def test_first_parent_limit_1():
+    lcm = LCMNeighbours(min_supp=3)
+    lcm.fit(D)
+
+    limit = 1
+    tids = lcm.item_to_tids_[limit]
+
+    ## pattern = {4, 6} -> first parent OK
+    itemset, tids, _ = next(lcm._inner((frozenset([4, 6]), tids), limit), NULL_RESULT)
+    assert itemset == (1, 4, 6)
+    assert len(tids) == 3
+
+    # pattern = {} -> first parent fails
+    itemset, tids, _ = next(lcm._inner((frozenset(), tids), limit), NULL_RESULT)
+    assert itemset == None
+
+
+def test_first_parent_limit_2():
+    lcm = LCMNeighbours(min_supp=3)
+    lcm.fit(D)
+
+    # pattern = {} -> first parent OK
+    tids = lcm.item_to_tids_[2]
+    itemset, tids, _ = next(lcm._inner((frozenset(), tids), 2), NULL_RESULT)
+    assert itemset == (2,)
+    assert len(tids) == 5
+
+    # pattern = {4} -> first parent OK
+    tids = lcm.item_to_tids_[2] & lcm.item_to_tids_[4]
+    itemset, tids, _ = next(lcm._inner((frozenset([4]), tids), 2), NULL_RESULT)
+    assert itemset == (2, 4)
+    assert len(tids) == 3
+
+
+def test_first_parent_limit_3():
+    lcm = LCMNeighbours(min_supp=3)
+    lcm.fit(D)
+
+    tids = lcm.item_to_tids_[3]
+    itemset, tids, _ = next(lcm._inner((frozenset(), tids), 3), NULL_RESULT)
+    assert itemset == (3,)
+    assert len(tids) == 3
+
+
+def test_first_parent_limit_4():
+    lcm = LCMNeighbours(min_supp=3)
+    lcm.fit(D)
+
+    tids = lcm.item_to_tids_[4]
+    itemset, tids, _ = next(lcm._inner((frozenset(), tids), 4), NULL_RESULT)
+    assert itemset == (4,)
+    assert len(tids) == 5
+
+
+def test_first_parent_limit_5():
+    lcm = LCMNeighbours(min_supp=3)
+    lcm.fit(D)
+
+    tids = lcm.item_to_tids_[5]
+    itemset, tids, _ = next(lcm._inner((frozenset(), tids), 5), NULL_RESULT)
+    assert itemset == (2, 5)
+    assert len(tids) == 4
+
+
+def test_first_parent_limit_6():
+    lcm = LCMNeighbours(min_supp=3)
+    lcm.fit(D)
+
+    tids = lcm.item_to_tids_[6]
+    itemset, tids, _ = next(lcm._inner((frozenset(), tids), 6), NULL_RESULT)
+    assert itemset == (4, 6)
+    assert len(tids) == 4
+
+
+def test_lcm_empty_fit():
+    # 1. test with a min_supp high above the maximum supp
+    lcm = LCMNeighbours(min_supp=100)
+    res = lcm.fit_discover(D)
+    assert isinstance(res, pd.DataFrame)
+    assert res.empty, f"{res}"
+
+    # 2. test with empty data
+    lcm = LCMNeighbours(min_supp=3)
+    res = lcm.fit_discover([])
+    assert isinstance(res, pd.DataFrame)
+    assert res.empty
+
+
+def test_lcm_discover():
+    lcm = LCMNeighbours(min_supp=3)
+    patterns = lcm.fit_discover(D)  # get new pattern set
+
+    for itemset, true_itemset in zip(patterns.itemset, true_patterns.itemset):
+        assert itemset == true_itemset
+    pd.testing.assert_series_equal(
+        patterns.support, true_patterns.support, check_dtype=False
+    )
+
+
+def test_lcm_discover_max_depth():
+    patterns = LCMNeighbours(min_supp=3, max_depth=1).fit_discover(D, return_depth=True)
+    assert not ((1, 4, 6) in patterns.itemset.tolist())
+    assert (patterns.depth < 2).all()
+
+
+def test_relative_support():
+    lcm = LCMNeighbours(min_supp=0.4)  # 40% out of 7 transactions ~= 3
+    lcm.fit(D)
+    np.testing.assert_almost_equal(lcm._min_supp, 2.8, 2)
+
+    for item in lcm.item_to_tids_.keys():
+        assert set(lcm.item_to_tids_[item]) == true_item_to_tids[item]
+
